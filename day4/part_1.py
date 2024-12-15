1e027a11fe9fc660a33b33fc2905f021cda00f8a
Ladislav Lučivjanský <l.lucivjansky@gmail.com>
2024-12-15 15:27:19 +0000

    day4 day 1 not done

--- day4/part_1.py
+++ day4/part_1.py
@@ -6,8 +6,17 @@ import re
 
 def main():
     """ Main program """
-    with open("day3/input.txt", "r") as f:
-        input = f.read()
+    game_field = []
+    with open("day4/input.txt", "r") as f:
+        for line in f:    
+            game_field.append(line.replace("\n", ""))
+    xmas_string = game_field
+    xmas_string_reversed = game_field[::-1]
+    xmas_string_diag = [tup[0] for tup in list(zip(game_field))]
+    xmas_string_diag_reserved = [tup[0] for tup in list(zip(xmas_string_reversed))]
+    xmas_string_joined = xmas_string + xmas_string_reversed + xmas_string_diag + xmas_string_diag_reserved
+    xmas_string_found = re.findall("XMAS", ' '.join(xmas_string_joined))
+    print(len(xmas_string_found))
 
 if __name__ == "__main__":
     main()
